e391204652f842936b6e891fc1af52f25d0b981c
Keyvan Sadeghi <ksa128@uit.no>
2024-09-09 17:53:50 +0200

    Can now extract sql answer from task file

--- Scripts/task_handler.go
+++ Scripts/task_handler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main()  {
@@ -16,12 +17,29 @@ func main()  {
 	}
 
 	fmt.Println(string(content))
-}
-
-func write_answer() {
+	answer := get_answer(string(content))
+	fmt.Printf("Answer:%s, Length: %d", answer, len(answer))
 	
 }
 
-func get_answer() {
+// func write_answer(data string) {
+	// 	answer_header := "### Answer here:"
+	
+	// }
+	
+func get_answer(data string) string {
+	answer_header := "### Answer here:"
+	header_index := strings.Index(data, answer_header)
 
+	// Get the index of header to procees extracting answer
+	if header_index == -1 {
+		fmt.Printf("File does not contain the given header: %s", answer_header)
+		return ""
+	}
+
+	// Get the answer
+	start_index := header_index + len(answer_header)
+	content := data[start_index:]
+
+	return content
 }
