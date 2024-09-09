888c9050f9f439101da3deec0d668556bb476e78
Keyvan Sadeghi <ksa128@uit.no>
2024-09-09 21:22:30 +0200

    Can now write to the sql file

--- Scripts/task_handler.go
+++ Scripts/task_handler.go
@@ -8,24 +8,29 @@ import (
 
 func main()  {
 	fmt.Println("Starting up...")
-
 	file := "Static/Problems/1_0_Easy/1_1.md"
-	content, err := os.ReadFile(file)
 
-	if err != nil {
-		panic(err)
-	}
+	content, err := os.ReadFile(file)
+	if err != nil { panic(err) }
+	
+	answer := get_answer(string(content))
 
 	fmt.Println(string(content))
-	answer := get_answer(string(content))
 	fmt.Printf("Answer:%s, Length: %d", answer, len(answer))
+
+	my_queries_file := "SQL_Setup/my_queries.sql"
+	err = write_answer(answer, my_queries_file)
+	if err != nil { fmt.Printf("Error when writing to sql file: %s", err)}
 	
 }
 
-// func write_answer(data string) {
-	// 	answer_header := "### Answer here:"
-	
-	// }
+func write_answer(query string, file_name string) error {
+	data := []byte(query)
+	err := os.WriteFile(file_name, data, 0777)
+
+	if err != nil { return err }
+	return nil
+}
 	
 func get_answer(data string) string {
 	answer_header := "### Answer here:"
